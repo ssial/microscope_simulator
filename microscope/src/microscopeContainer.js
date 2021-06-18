20428f59538da244091b379f2d347cf6576ccca5
ssial <ssial@uoguelph.ca>
2021-06-18 13:46:37 -0400

    can change src of image based on which lens is selected

--- microscope/src/microscopeContainer.js
+++ microscope/src/microscopeContainer.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import specimen from "./imgs/thumbnail_Scale_4X.jpg";
+import specimen1 from "./imgs/thumbnail_Scale_4X.jpg";
+import specimen2 from "./imgs/thumbnail_Scale_10X.jpg";
+import specimen3 from "./imgs/thumbnail_Scale_40X.jpg";
 
 export default class MicroscopeContainer extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { top: 100, right: 25, brightness: "100%" }
+        this.state = { top: 100, right: 25, brightness: 100, blur: 0, lens: "4x", src: specimen1 }
     }
 
     handleChange = event => {
@@ -47,25 +49,49 @@ export default class MicroscopeContainer extends React.Component {
         }
         else if (event.target.id === "incBrightness") {
             console.log("increase Brightness")
-            let brightness = parseInt(this.state.brightness) + 25;
-            if(brightness <= 200) {
-                let brightnessPercentage = brightness.toString() + "%";
+            let brightnessP = this.state.brightness + 25;
+            if (brightnessP <= 200) {
                 this.setState({
-                    brightness: brightnessPercentage
+                    brightness: brightnessP
                 })
             }
         }
         else if (event.target.id === "decBrightness") {
             console.log("decrease Brightness")
-            let brightness = parseInt(this.state.brightness) - 25;
-            if(brightness >= 25) {
-                let brightnessPercentage = brightness.toString() + "%";
+            let brightnessP = this.state.brightness - 25;
+            if (brightnessP >= 25) {
                 this.setState({
-                    brightness: brightnessPercentage
+                    brightness: brightnessP
                 })
             }
         }
-     
+        else if (event.target.id === "incBlur") {
+            // console.log("decrease Brightness")
+            // let brightnessP = this.state.brightness - 25;
+            // if (brightnessP >= 25) {
+            //     this.setState({
+            //         brightness: brightnessP
+            //     })
+            // }
+        }
+        else if (event.target.id === "4x") {
+            this.setState({
+                lens: "4x",
+                src: specimen1
+            })
+        }
+        else if (event.target.id === "10x") {
+            this.setState({
+                lens: "10x",
+                src: specimen2
+            })
+        }
+        else if (event.target.id === "40x") {
+            this.setState({
+                lens: "40x",
+                src: specimen3
+            })
+        }
 
     };
 
@@ -81,22 +107,34 @@ export default class MicroscopeContainer extends React.Component {
     render() {
         return (
             <div>
-                <div style={{width:"100%", display:"flex", justifyContent:"center"}}>
-                    <div style={{ width: "800px", height: "600px", border: "solid black 1px" }} onKeyPress={this.handleKeyPress}><img src={specimen} style={{ width: "450px", position: "relative", top: this.state.top, right: this.state.right, filter: "brightness("+this.state.brightness+")"}} alt="Comb under microscope at 4x" />
+                <div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
+                    <div style={{ width: "800px", height: "600px", border: "solid black 1px" }} onKeyPress={this.handleKeyPress}><img src={this.state.src} style={{ width: "450px", position: "relative", top: this.state.top, right: this.state.right, filter: "brightness(" + this.state.brightness + "%)" }} alt="Comb under microscope at 4x" />
                     </div>
                 </div>
-                <div style={{width:"100%", display:"flex", justifyContent:"center"}}>
-                <div style={{width:"200px", border:"solid black 1px", display:"flex", justifyContent:"space-around"}}>
-                    <div>Brightness: </div>
-                    <button id="incBrightness" onClick={this.handleChange} type="button">Up</button>
-                    <button id="decBrightness" onClick={this.handleChange} type="button">Down</button>
-                </div>
-                <div style={{width:"200px", border:"solid black 1px", display:"flex", justifyContent:"space-around"}}>
-                    <button id="top" onClick={this.handleChange} type="button">Top</button>
-                    <button id="left" onClick={this.handleChange} type="button">Left</button>
-                    <button id="right" onClick={this.handleChange} type="button">Right</button>
-                    <button id="down" onClick={this.handleChange} type="button">Down</button>
-                </div>
+                <div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
+                    <div style={{ width: "225px", display: "flex", justifyContent: "space-around", margin: 5 }}>
+                        <div>Brightness: {this.state.brightness}%</div>
+                        <button id="incBrightness" onClick={this.handleChange} type="button">Up</button>
+                        <button id="decBrightness" onClick={this.handleChange} type="button">Down</button>
+                    </div>
+                    <div style={{ width: "245px", display: "flex", justifyContent: "space-around", margin: 5 }}>
+                        <div>Move: </div>
+                        <button id="top" onClick={this.handleChange} type="button">Top</button>
+                        <button id="left" onClick={this.handleChange} type="button">Left</button>
+                        <button id="right" onClick={this.handleChange} type="button">Right</button>
+                        <button id="down" onClick={this.handleChange} type="button">Down</button>
+                    </div>
+                    <div style={{ width: "170px", display: "flex", justifyContent: "space-around", margin: 5 }}>
+                        <div>Lens: </div>
+                        <button id="4x" onClick={this.handleChange} type="button">4x</button>
+                        <button id="10x" onClick={this.handleChange} type="button">10x</button>
+                        <button id="40x" onClick={this.handleChange} type="button">40x</button>
+                    </div>
+                    <div style={{ width: "130px", display: "flex", justifyContent: "space-around", margin: 5 }}>
+                        <div>Blur: </div>
+                        <button id="incBlur" onClick={this.handleChange} type="button">Up</button>
+                        <button id="decBlur" onClick={this.handleChange} type="button">Down</button>
+                    </div>
                 </div>
             </div>
         );
